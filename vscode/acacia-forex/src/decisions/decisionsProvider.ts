0a6dbac10e2a034b92de3450456732116a563843
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2025-02-09 21:11:36 +0200

    create decisions provider

---
+++ vscode/acacia-forex/src/decisions/decisionsProvider.ts
@@ -0,0 +1,43 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
+export class DecisionsProvider implements vscode.WebviewViewProvider {
+    public static readonly viewType = 'acacia-forex.decisions';
+
+    constructor(private context: vscode.ExtensionContext) {}
+
+    public resolveWebviewView(
+        webviewView: vscode.WebviewView,
+        context: vscode.WebviewViewResolveContext,
+        _token: vscode.CancellationToken
+    ): void {
+        webviewView.webview.options = {
+            enableScripts: true
+        };
+
+        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+        webviewView.webview.onDidReceiveMessage(
+            message => {
+                switch (message.command) {
+                    case 'openForm':
+                        vscode.window.showInformationMessage('Opening form');
+                        break;
+                }
+            },
+            undefined,
+            this.context.subscriptions
+        );   
+    }
+
+    private _getHtmlForWebview(webview: vscode.Webview): string {
+        const htmlPath = path.join(this.context.extensionPath, 'resources', 'decisions.html');
+        const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+
+        return htmlContent;
+    }
+
+ 
+}
+
